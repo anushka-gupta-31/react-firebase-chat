74f9fbadae96c7ded7312d255da2599b370fff49
Anushka Gupta <anushka.mini31@gmail.com>
2025-07-09 17:11:06 +0530

    Application design

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Lama Dev Chat App Starter template
+# Anushka's Chat Application
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a chat application which can be used for real-time messaging.
---
+++ src/components/list/List.jsx
@@ -0,0 +1,15 @@
+import "./list.css";
+import UserInfo from "./userInfo/userInfo";
+import ChatList from "./chatList/ChatList"
+
+const List = () => {
+    return (
+        <div className='list'>
+            <UserInfo/>
+            <ChatList/>
+
+        </div>  
+    )
+}
+
+export default List

8c408cdfeb8c4b5ce55b5a9abe082f452f3e3e47
Anushka Gupta <anushka.mini31@gmail.com>
2025-07-09 20:40:26 +0530

    updated path for lib

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -8,10 +8,10 @@ import {
   onSnapshot,
   updateDoc,
 } from "firebase/firestore";
-import { db } from "../../lib/firebase";
-import { useChatStore } from "../../lib/chatStore";
-import { useUserStore } from "../../lib/userStore";
-import upload from "../../lib/upload";
+import { db } from "../lib/firebase";
+import { useChatStore } from "../lib/chatStore";
+import { useUserStore } from "../lib/userStore";
+import upload from "../lib/upload";
 import { format } from "timeago.js";
 
 const Chat = () => {
